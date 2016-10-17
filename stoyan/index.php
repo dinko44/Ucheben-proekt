a5982cfc31a32f65fb82131df8ff97f945951ea1
root <gioko_9@abv.bg>
2016-10-17 16:31:40 +0300

    first commit

---
+++ stoyan/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo "Hello world from Stoyan!";
+        echo "Promiana v proekta!";
+        echo "Message from Stoyan: I am adding this line on my computer!";
+        ?>
+    </body>
+</html>
